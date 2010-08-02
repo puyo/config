5ac2c54fb70747622d325dbeb80390ea052b995c
Gregory McIntyre <greg@gregorymcintyre.com>
2010-08-02 23:25:31 +1000

    Updated packages.list for Xfce4 and Lucid.

--- apt.sh
+++ apt.sh
@@ -1,7 +1,7 @@
 sudo cp sources.list.d/*.list /etc/apt/sources.list.d/
 
 wget -q http://wine.budgetdedicated.com/apt/387EE263.gpg -O- | sudo apt-key add -
-sudo wget http://wine.budgetdedicated.com/apt/sources.list.d/jaunty.list -O /etc/apt/sources.list.d/winehq.list
+sudo wget http://wine.budgetdedicated.com/apt/sources.list.d/lucid.list -O /etc/apt/sources.list.d/winehq.list
 
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0x1d7e9dd033e89ba781e32a24b9f1c432ae74ae63
 
@@ -9,6 +9,4 @@ wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
 
 sudo aptitude update
 sudo aptitude safe-upgrade
-cat package.list | sudo xargs aptitude -y install
-
-sudo dontzap -d
+cat package.list | sudo xargs aptitude -y --without-recommends install
