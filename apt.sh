aa07d5c51173a37bef63484be6c489d7cf363c82
Gregory McIntyre <greg@gregorymcintyre.com>
2013-08-22 23:13:29 +1000

    Update package list.

--- apt.sh
+++ apt.sh
@@ -1,7 +1,5 @@
 sudo cp sources.list.d/*.list /etc/apt/sources.list.d/
-
 wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
-
-sudo aptitude update
-sudo aptitude safe-upgrade
+sudo apt-get update
+sudo apt-get install aptitude
 cat packages.list | sudo xargs aptitude -y --without-recommends install
