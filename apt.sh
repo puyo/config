04fe29b8df0639b2ccf519eeacfda28e3171b692
Gregory McIntyre <greg@gregorymcintyre.com>
2013-08-26 20:15:24 +1000

    Music packages.

--- apt.sh
+++ apt.sh
@@ -1,5 +1,10 @@
-sudo cp sources.list.d/*.list /etc/apt/sources.list.d/
+sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
 wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
+
+sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+
 sudo apt-get update
 sudo apt-get install aptitude
+
 cat packages.list | sudo xargs aptitude -y --without-recommends install
