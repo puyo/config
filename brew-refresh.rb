91a447a99d318efd1bacdfc435e3649ed6fa71c8
Gregory McIntyre <greg@gregorymcintyre.com>
2020-05-01 16:09:02 +1000

    Fix brew refresh script

--- brew-refresh.rb
+++ brew-refresh.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+#
+# Use brew-list.txt as a master list of packages to ensure are installed. Any
+# brew packages NOT in this file will be uninstalled (after a prompt).
+#
+# Use brew-cask-list.txt as a master list of brew cask packages to ensure are
+# installed. No casks are uninstalled at this time.
 
 require 'pp'
 
@@ -12,8 +18,21 @@ to_remove = installed - wanted
 to_install = wanted - installed
 
 system('brew', 'update')
-system('brew', 'uninstall', '--ignore-dependencies', '--force', *to_remove) if to_remove.any?
-system('brew', 'install', *wanted) if wanted.any?
+if to_remove.any?
+  begin
+    puts to_remove.join(' ') + "\n\nRemove these packages? (y/N) "
+    x = gets
+  end until x.strip.downcase == 'y'
+  system('brew', 'uninstall', '--ignore-dependencies', '--force', *to_remove)
+end
+
+if to_install.any?
+  begin
+    puts to_install.join(' ') + "\n\nInstall these packages? (y/N) "
+    x = gets
+  end until x.strip.downcase == 'y'
+  system('brew', 'install', *to_install)
+end
 system('brew', 'upgrade')
 system('brew', 'cleanup')
 
