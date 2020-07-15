10a347c156c791f815300326937acec783440b6e
Gregory McIntyre <greg@gregorymcintyre.com>
2020-07-16 07:28:39 +1000

    Quote aws exclude/include patterns

--- backup/sync-up.rb
+++ backup/sync-up.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-system("aws s3 sync ./ s3://puyofiles/ --profile puyo --exclude photos/* --delete")
+system("aws s3 sync ./ s3://puyofiles/ --profile puyo --exclude 'photos/*' --delete")
