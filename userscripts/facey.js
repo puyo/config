821fae9145821f5b4cbb95214c612732985c9a7b
Gregory McIntyre <greg@gregorymcintyre.com>
2021-06-15 08:12:48 +1000

    Improve facebook ad remover

--- userscripts/facey.js
+++ userscripts/facey.js
@@ -3,7 +3,7 @@
 // @namespace    puyo/facey
 // @license      Creative Commons BY-NC-SA
 // @encoding     utf-8
-// @version      1.2
+// @version      1.3
 // @description  Make Facey better
 // @author       puyo
 // @match        https://www.facebook.com/
@@ -17,20 +17,31 @@
 
     let observer
     const feed = document.querySelector('[role=feed]')
-    const mutationObserverConfig = { attributes: false, childList: true, subtree: false };
+    const mutationObserverConfig = { attributes: false, childList: true, subtree: true }
+    let count = 0
 
-    const callback = (mutList, obs) => {
-        observer.disconnect();
-        const articles = feed.querySelectorAll('[role=article]')
+    const removeAds = () => {
+        const articles = feed.querySelectorAll('[role=article]:not([data-checked])')
+        //console.log(`removing ads from ${articles.length} articles, ${count} removed so far`)
         articles.forEach(article => {
-            const sponsored = article.querySelector('a[aria-label=Sponsored][role=link]');
+            const sponsored = article.querySelector('a[aria-label=Sponsored][role=link]')
             if (sponsored) {
-                article.style.display = 'none';
+                count += 1
+                article.parentNode.removeChild(article)
+                //article.style.display = 'none'
             }
+            article.setAttribute('data-checked', 'true')
         })
-        observer.observe(feed, mutationObserverConfig);
     }
 
-    observer = new MutationObserver(callback);
-    observer.observe(feed, mutationObserverConfig);
+    const callback = (mutList, obs) => {
+        observer.disconnect()
+        removeAds()
+        observer.observe(feed, mutationObserverConfig)
+    }
+    removeAds()
+
+    observer = new MutationObserver(callback)
+    observer.observe(feed, mutationObserverConfig)
 })();
+
