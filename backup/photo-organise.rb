382988c01db85244c81d9184a73ee14cae070cd7
Gregory McIntyre <greg@gregorymcintyre.com>
2021-08-13 11:37:21 +1000

    Update photo organise script

--- backup/photo-organise.rb
+++ backup/photo-organise.rb
@@ -23,32 +23,34 @@ require 'progress'
 require 'yaml'
 require 'exif'
 require 'date'
+require 'digest'
 
 # To run:
 #
 #     Organise.new.organise
 #
 class Organise
-  DATE_RE = /^\d{4}-\d{2}-\d{2}/
+  RE_DATE = /^(?:19|20)\d{2}-\d{2}-\d{2}/
+  RE_MONTH = /^(?:19|20)\d{2}-\d{2}/
 
-  def add_new_checksums
-    paths.with_progress('checksumming').each do |path|
-      next if checksums.key?(path)
-
-      checksums[path] = Digest::MD5.file(path).to_s
+  def add_new_info
+    paths.with_progress("checking #{paths.size} files").each do |path|
+      info[path] = update_photo_info(info[path] || {}, path)
     end
   end
 
-  def add_new_dates
-    paths.with_progress('dating').each do |path|
-      next if dates.key?(path)
-
-      dates[path] = calc_date(path)
-    end
+  def calc_sum(path)
+    Digest::MD5.file(path).to_s
   end
 
   def calc_date(path)
-    calc_filename_date(path) || calc_exif_date(path) || '?'
+    calc_filename_date(path) || calc_exif_date(path) || calc_related_date(path) || calc_same_day_date(path)
+  end
+
+  def calc_dupes
+    dupes = Hash.new { |h, k| h[k] = [] }
+    info.each { |path, _photo| dupes[photo['sum']] << path }
+    dupes.values.select { |paths| paths.size > 1 }
   end
 
   def calc_exif_date(path)
@@ -61,94 +63,172 @@ class Organise
   end
 
   def calc_filename_date(path)
-    basename = File.basename(path)
-    basename[0, 10] if DATE_RE.match?(basename)
+    path.split(File::SEPARATOR).reverse.each do |seg|
+      return seg[0, 10] if RE_DATE.match?(seg)
+      return "#{seg[0, 7]}-01" if RE_MONTH.match?(seg)
+    end
+    nil
   rescue Date::Error
     nil
   end
 
-  def checksums
-    @checksums ||= load_checksums || {}
-  end
-
-  def checksums_path
-    'photo-checksums.yml'
-  end
-
-  def dates
-    @dates ||= load_dates || {}
-  end
-
-  def dates_path
-    'photo-dates.yml'
-  end
-
-  def dest_dir(date)
-    if date == '?'
-      '_date_unknown'
-    else
-      date[0, 7]
+  def calc_related_date(path)
+    calc_related_paths(path).each do |related_path|
+      result = calc_exif_date(related_path)
+      return result if result
     end
+    nil
   end
 
-  def load_checksums
-    YAML.load_file(checksums_path) if File.exist?(checksums_path)
+  # Calculate a file's date by comparing for example IMG_123.JPG with
+  # IMG_122.JPG and IMG_124.JPG - if files on either side in order have
+  # the same date, then assume that date for this file too.
+  def calc_same_day_date(path)
+    ext = File.extname(path)
+    base = File.basename(path, ext)
+    dir = File.dirname(path)
+    base_glob = base.gsub(/\d/, '[0-9]')
+    glob = File.join(dir, "#{base_glob}.*")
+    paths = Dir.glob(glob).sort
+    index = paths.index(path)
+    d0 = (index - 1).downto(0).lazy.filter_map { |i| calc_exif_date(paths[i]) }.first
+    d1 = (index + 1).upto(paths.size - 1).lazy.filter_map { |i| calc_exif_date(paths[i]) }.first
+    return d0 if d0 && d1
+
+    nil
   end
 
-  def load_dates
-    YAML.load_file(dates_path) if File.exist?(dates_path)
+  def calc_related_paths(path)
+    ext = File.extname(path)
+    base = File.basename(path, ext)
+    dir = File.dirname(path)
+    glob = File.join(dir, [base, '*'].join('.'))
+    Dir.glob(glob) - [path]
   end
 
-  def move_into_date_dirs
-    dates.each do |path, date|
-      # p path: path, date: date
-
-      basename = File.basename(path)
-      dir = dest_dir(date)
-      # current_dir = File.dirname(path)
-      desired_path = "photos/#{dir}/#{basename}"
-      p path => desired_path
-    end
-  end
-
-  def organise
-    puts "#{paths.size} files..."
-    # update_checksums
-    update_dates
-  end
-
-  def paths
-    @paths ||= Dir.glob('photos/**/*').select { |path| File.exist?(path) && !Dir.exist?(path) }
-  end
-
-  def update_dates
-    dates.delete_if { |path, _md5| !File.exist?(path) }
-    add_new_dates
-    File.open(dates_path, 'w') { |f| YAML.dump(dates, f) }
-    pp dates.values.map { |date| dest_dir(date) }.uniq.sort
-    pp dates.group_by { |_path, date| dest_dir(date) }.map { |dir, files| [dir, files.size] }.sort
-
-    # move_into_date_dirs
-  end
-
-  def update_checksums
-    checksums.delete_if { |path, _md5| !File.exist?(path) }
-    add_new_checksums
-    File.open(checksums_path, 'w') { |f| YAML.dump(checksums, f) }
-
-    dupes = Hash.new { |h, k| h[k] = [] }
-    checksums.each do |path, md5|
-      dupes[md5] << path
-    end
-    dupes = dupes.values.select { |paths| paths.size > 1 }
-
-    dupes.each do |paths|
+  def dedupe
+    calc_dupes.each do |paths|
       exists = paths.find { |path| File.exist?(path) }
       if exists
         redundant = paths - [exists]
         redundant.each { |path| FileUtils.rm_rf(path, verbose: true, noop: ENV['FORREALZ'] != '1') }
       end
     end
+  end
+
+  def dest_base(path, date)
+    base = File.basename(path)
+    return base if date.nil?
+
+    prefix = "#{date}_"
+    return base if base.start_with?(prefix)
+
+    "#{prefix}#{base}"
+  end
+
+  def dest_dir(date)
+    if date
+      date[0, 7]
+    else
+      '0_unknown_date'
+    end
+  end
+
+  def info_path
+    'photo-info.yml'
+  end
+
+  def info
+    @info ||= load_info
+  end
+
+  def load_info
+    if File.exist?(info_path)
+      YAML.load_file(info_path)
+    else
+      {}
+    end
+  end
+
+  def move_into_date_dirs
+    paths.each do |path|
+      photo = info[path]
+      date = photo['date']
+      dest_base = dest_base(path, date)
+      dest_dir = dest_dir(date)
+      desired_path = "photos/#{dest_dir}/#{dest_base}"
+
+      # p path: path, desired_path: desired_path
+    end
+  end
+
+  # 1. Sync down a subset of all photos (current year + previous year, by
+  #    default)
+  #
+  # 2. Load info for year (yml/csv)
+  #
+  # 3. Select info (IF it would be in the subset of all photos, and file
+  #    exists)
+  #
+  # 4. Add new info for any new files in directory
+  #
+  # 5. Move all files into their correct location if they are not there
+  #    already (YYYY/YYYY-MM-DD_<existing name>.<existing ext>), updating
+  #    info
+  #
+  # 6. Sync up the subset of photos with --delete
+  #
+  def organise
+    update_info
+
+    # dedupe
+
+    # pp info.values.map { |photo| dest_dir(photo['date']) }.uniq.sort
+    # pp info.group_by { |_path, photo| dest_dir(photo['date']) }.map { |dir, files| [dir, files.size] }.sort
+
+    # move_into_date_dirs
+  end
+
+  def paths
+    @paths ||= Dir.glob('photos/**/*').select { |path| File.exist?(path) && !Dir.exist?(path) }
+  end
+
+  def update_info
+    info.delete_if { |path, _photo_info| !File.exist?(path) }
+    add_new_info
+    File.open(info_path, 'w') { |f| YAML.dump(info, f) }
+  end
+
+  def update_photo_date(photo, path)
+    case ENV['DATE']
+    when 'force'
+      date = calc_date(path)
+      photo['date'] = calc_date(path) if date
+    when 'skip'
+      # OK
+    else
+      date = calc_date(path)
+      photo['date'] ||= date if date
+    end
+    photo
+  end
+
+  def update_photo_info(photo, path)
+    update_photo_sum(photo, path)
+    update_photo_date(photo, path)
+    photo
+  end
+
+  def update_photo_sum(photo, path)
+    case ENV['SUM']
+    when 'force'
+      photo['sum'] = calc_sum(path)
+    when 'skip'
+      # OK
+    else
+      photo['sum'] ||= calc_sum(path)
+    end
+    photo
   end
 end
 
