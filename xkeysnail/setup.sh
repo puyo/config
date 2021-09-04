9f4bbc90fb213fb70eccfcd02b99ec78c2e079f2
Gregory McIntyre <greg@gregorymcintyre.com>
2021-09-05 06:57:56 +1000

    Ditch xkeysnail and update gvim to work with autokey

--- xkeysnail/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo service xkeysnail stop
-sudo cp xkeysnail.init.d.sh /etc/init.d/xkeysnail
-sudo mkdir -p /etc/xkeysnail
-sudo cp config.py /etc/xkeysnail/config.py
-sudo chown root:root /etc/init.d/xkeysnail
-sudo update-rc.d xkeysnail defaults
-sudo service xkeysnail restart
