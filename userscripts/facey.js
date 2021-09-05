ff1027a9b61badff8f551ec1853354ea8e7b0ac8
Gregory McIntyre <greg@gregorymcintyre.com>
2021-09-05 11:02:53 +1000

    Facebook ad detection cat and mouse

--- userscripts/facey.js
+++ userscripts/facey.js
@@ -3,7 +3,7 @@
 // @namespace    puyo/facey
 // @license      Creative Commons BY-NC-SA
 // @encoding     utf-8
-// @version      1.5
+// @version      1.6
 // @description  Make Facey better
 // @author       puyo
 // @match        https://www.facebook.com/
@@ -19,12 +19,30 @@
     const mutationObserverConfig = { attributes: false, childList: true, subtree: true }
     let count = 0
 
+    const textForNode = (x) => {
+        if (x.data) {
+            return x.data // #text nodes
+        }
+        if (x.attributes && x.attributes.style == null) {
+            return x.textContent // <span> nodes
+        }
+        return '' // <span style=...> nodes
+    }
+
     const removeAds = () => {
         const articles = document.querySelectorAll('[role=feed] [role=article]:not([data-checked])')
-        console.log(`removing ads from ${articles.length} articles, ${count} removed so far`)
         articles.forEach(article => {
-            const sponsored = article.querySelector('a[aria-label=Sponsored][role=link]')
-            if (sponsored) {
+            const sponsored = article.querySelector('[aria-label=Sponsored]')
+            let ad = false
+            if (sponsored != null) {
+                const h4 = article.querySelector('h4')
+                const title = h4 && h4.textContent
+                const textNodes = sponsored.parentNode.childNodes[1].childNodes
+                const text = Array.from(textNodes).map(x => textForNode(x)).join('')
+                ad = (text === 'Sponsored')
+                console.log('Ad detector', {title, text, ad})
+            }
+            if (ad) {
                 count += 1
                 article.parentNode.removeChild(article)
             } else {
