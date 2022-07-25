e4a0788d457ee4d4895c4158f28973e8db0a1d5e
Gregory McIntyre <greg@gregorymcintyre.com>
2022-07-25 12:54:10 +1000

    Update facebook ad blocker

--- userscripts/facey.js
+++ userscripts/facey.js
@@ -2,8 +2,7 @@
 // @name         Facey
 // @namespace    puyo/facey
 // @license      Creative Commons BY-NC-SA
-// @encoding     utf-8
-// @version      1.11
+// @version      1.12
 // @description  Make Facey better
 // @author       puyo
 // @match        https://www.facebook.com/
@@ -12,99 +11,103 @@
 // @downloadURL  https://raw.githubusercontent.com/puyo/config/master/userscripts/facey.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    let observer
-    const mutationObserverConfig = { attributes: false, childList: true, subtree: true }
-    window.count = 0
+  let observer;
+  const mutationObserverConfig = {
+    attributes: false,
+    childList: true,
+    subtree: true,
+  };
+  window.count = 0;
 
-    let textForNode, textForChildren
-    let removeAds, removeAd, sponsoredNodes, isSponsoredElem
+  let textForNode, textForChildren;
+  let removeAds, removeAd, hasAriaLabelSponsored;
 
-    isSponsoredElem = (node) => {
-        const style = getComputedStyle(node)
-        if (node.data || style.display === "none" || style.top !== "0px" || style.display === "flex") {
-            return false
+  const consoleStyle = "background-color: darkblue; color: white;";
+  const adStyle = "background-color: darkred; color: white;";
+
+  hasAriaLabelSponsored = window.hasAriaLabelSponsored = (article, debug) => {
+    let found = false;
+    if (article.querySelector("[aria-label=Sponsored]")) {
+      return true;
+    }
+    const descTextSponsored = (id) => {
+      if (id == null || id === "") {
+        return false;
+      }
+      const descNode = document.querySelector(`#${id}`);
+      const text = descNode && descNode.textContent;
+      return text && text.includes("Sponsored");
+    };
+    (article.getAttribute("aria-describedby") || "")
+      .split(" ")
+      .forEach((id) => {
+        found = descTextSponsored(id);
+        if (found) {
+          return found;
         }
-        return true
+      });
+    article.querySelectorAll("[aria-labelledby]").forEach((n) => {
+      const id = n.getAttribute("aria-labelledby");
+      found = descTextSponsored(id);
+      if (found) {
+        return found;
+      }
+    });
+    return found;
+  };
+
+  removeAd = window.removeAd = (article, debug = false) => {
+    let ad = false;
+    const h4 = article.querySelector("h4");
+    const title = h4 && h4.textContent;
+    if (hasAriaLabelSponsored(article, debug)) {
+      ad = true;
     }
-
-    textForNode = (node) => {
-        return node.data || node.textContent
+    console.log("%cFacebook Ad?", ad ? adStyle : consoleStyle, {
+      title,
+      article,
+      ad,
+    });
+    if (ad) {
+      if (window.count == null) {
+        window.count = 0;
+      }
+      window.count += 1;
+      if (debug) {
+        article.style.outline = "5px solid red";
+      } else {
+        article.parentNode.removeChild(article);
+      }
+    } else {
+      article.setAttribute("data-checked", "true");
     }
+  };
 
-    textForChildren = (node) => {
-        const elems =
-          Array.from(node.querySelectorAll('*'))
-          .filter(isSponsoredElem)
-          .sort((a, b) => parseInt(a.style.order) - parseInt(b.style.order))
-        return elems.slice(0, 9).map(textForNode).join('').replace(/-/g, '')
+  removeAds = window.removeAds = (debug = false) => {
+    let articles;
+    if (debug) {
+      console.log("%c-----", consoleStyle);
+      articles = document.querySelectorAll("[role=feed] [role=article]");
+    } else {
+      articles = document.querySelectorAll(
+        "[role=feed] [role=article]:not([data-checked])"
+      );
     }
+    articles.forEach((article) => {
+      removeAd(article, debug);
+    });
+  };
 
-    sponsoredNodes = (article, debug = false) => {
-        let result = []
-        const ariaLabelSponsoredNode = article.querySelector('[aria-label=Sponsored]')
-        if (ariaLabelSponsoredNode) {
-            result.push(ariaLabelSponsoredNode.parentNode)
-        }
-        result.push(article.querySelector('a[href^="/ads"]'))
-        result = result.concat(Array.from(article.querySelectorAll('div')))
-        return result
-    }
+  removeAds();
 
-    removeAd = (article, debug = false) => {
-        const nodes = sponsoredNodes(article, debug).filter(x => x != null)
-        let ad = false
-        const h4 = article.querySelector('h4')
-        const title = h4 && h4.textContent
-        for (let i = 0; i < nodes.length; i++) {
-            const node = nodes[i]
-            const text = textForChildren(node)
-            ad = text.includes('ponsored')
-            if (debug) {
-                console.log('Ad?', {title, node, text, ad})
-            }
-            if (ad) { break }
-        }
-        if (!debug) {
-            console.log('Ad?', {title, ad})
-        }
-        if (ad) {
-            if (window.count == null) {
-                window.count = 0
-            }
-            window.count += 1
-            article.parentNode.removeChild(article)
-        } else {
-            article.setAttribute('data-checked', 'true')
-        }
-    }
-
-    removeAds = (debug = false) => {
-        let articles
-        if (debug) {
-            console.log('-----')
-            articles = document.querySelectorAll('[role=feed] [role=article]')
-        } else {
-            articles = document.querySelectorAll('[role=feed] [role=article]:not([data-checked])')
-        }
-        articles.forEach(article => {
-            removeAd(article, debug)
-        })
-    }
-
-    window.textForChildren = textForChildren
-    window.removeAds = (debug = false) => removeAds(debug)
-
-    removeAds()
-
-    const callback = (_mutList, _obs) => {
-        observer.disconnect()
-        removeAds()
-        observer.observe(document, mutationObserverConfig)
-    }
-    observer = new MutationObserver(callback)
-    observer.observe(document, mutationObserverConfig)
+  const callback = (_mutList, _obs) => {
+    observer.disconnect();
+    removeAds();
+    observer.observe(document, mutationObserverConfig);
+  };
+  observer = new MutationObserver(callback);
+  observer.observe(document, mutationObserverConfig);
 })();
-
