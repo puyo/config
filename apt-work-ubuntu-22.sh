964573e9632bcee79e2821f7572a749a5ec622c9
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-06 12:09:36 +1000

    Update apt scripts

--- apt-work-ubuntu-22.sh
+++ apt-work-ubuntu-22.sh
@@ -2,49 +2,14 @@
 
 apt_install=sudo apt install -y --without-recommends
 
-# asdf elixir
-$(apt_install) autoconf fop libgl1-mesa-dev libglu1-mesa-dev libmysqlclient-dev libncurses5-dev libpng-dev libpq-dev libreadline-dev libssh-dev libwxgtk3.0-dev m4 unixodbc-dev xsltproc
-
-# asdf erlang
-$(apt_install) autoconf build-essential fop libgl1-mesa-dev libglu1-mesa-dev libncurses-dev libncurses5-dev libpng-dev libssh-dev libwxgtk-webview3.0-gtk3-dev libwxgtk3.0-gtk3-dev libxml2-utils m4 unixodbc-dev xsltproc
-
-# asdf ruby
-$(apt_install) autoconf bison build-essential libdb-dev libffi-dev libgdbm-dev libgdbm6 libncurses5-dev libreadline6-dev libssl-dev libyaml-dev uuid-dev zlib1g-dev
-
-# bevy
-$(apt_install) g++ pkg-config libx11-dev libasound2-dev libudev-dev
-
 # docker server
 $(apt_install) docker ca-certificates curl gnupg lsb-release
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo usermod -aG docker greg
 
-# ssh server
-$(apt_install) openssh-server
-
-# wmctrl (gvim wrapper)
-$(apt_install) wmctrl
-
-# jq (json pretty printer / query)
-$(apt_install) jq
-
-# postgresql server
-$(apt_install) postgresql libpq-dev
-
 # mysql server
 $(apt_install) mysql-server mysql-client-8.0 libmysqlclient-dev
 
 # redis server
 $(apt_install) redis-server redis-tools
-
-# htop
-$(apt_install) htop
-
-# diff-so-fancy
-sudo add-apt-repository ppa:aos1/diff-so-fancy
-$(apt_install) diff-so-fancy
-
-# github cli
-$(apt_install) gh
-
