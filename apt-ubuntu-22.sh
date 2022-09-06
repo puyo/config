412a1f08d9291848dc45564707e40c3b0f2035c7
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-06 14:52:56 +1000

    Update apt script

--- apt-ubuntu-22.sh
+++ apt-ubuntu-22.sh
@@ -1,6 +1,31 @@
 apt_install=sudo apt install -y --without-recommends
 
-$(apt_install) aptitude blender curl dict dosfstools emacs exuberant-ctags git htop imagemagick inkscape irssi manpages-dev ruby samba unrar unzip vim-gtk vlc wbritish wine zip
+# no recommends kthx
+sudo tee /etc/apt/apt.conf.d/99_norecommends << 'EOF'
+APT::Install-Recommends "false";
+APT::AutoRemove::RecommendsImportant "false";
+APT::AutoRemove::SuggestsImportant "false";
+EOF
+
+# unattended upgrades
+$(apt_install) unattended-upgrades
+
+# firefox (remove snap version, install apt version)
+sudo snap remove firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+$(apt_install) firefox
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | \
+  sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+# various
+$(apt_install) aptitude blender curl dict dosfstools emacs exuberant-ctags git \
+  htop imagemagick inkscape irssi manpages-dev ruby samba unrar unzip vim-gtk vlc \
+  wbritish wine zip
 
 # asdf elixir
 $(apt_install) autoconf fop libgl1-mesa-dev libglu1-mesa-dev libmysqlclient-dev libncurses5-dev libpng-dev libpq-dev libreadline-dev libssh-dev libwxgtk3.0-dev m4 unixodbc-dev xsltproc
@@ -36,4 +61,5 @@ $(apt_install) wmctrl
 # jq (json pretty printer / query)
 $(apt_install) jq
 
-
+# aws cli
+$(apt_install) awscli
