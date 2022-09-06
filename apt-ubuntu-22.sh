ca35aacdf72fad1445122fb42fa5df7ec1d4cd0d
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-06 12:07:35 +1000

    Update Ubuntu apt lists of packages

---
+++ apt-ubuntu-22.sh
@@ -0,0 +1,3 @@
+apt_install=sudo apt install -y --without-recommends
+
+$(apt_install) aptitude blender curl dict dosfstools emacs exuberant-ctags git htop imagemagick inkscape irssi manpages-dev ruby samba unrar unzip vim-gtk vlc wbritish wine zip
