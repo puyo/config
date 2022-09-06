7e74cef145e7672a221cfb6cdb853b41e99e3e41
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-06 16:22:45 +1000

    Lots of fixes

--- apt-ubuntu-22.sh
+++ apt-ubuntu-22.sh
@@ -1,4 +1,6 @@
-apt_install=sudo apt install -y --without-recommends
+#!/bin/bash
+
+set -xeuo pipefail
 
 # no recommends kthx
 sudo tee /etc/apt/apt.conf.d/99_norecommends << 'EOF'
@@ -8,12 +10,12 @@ APT::AutoRemove::SuggestsImportant "false";
 EOF
 
 # unattended upgrades
-$(apt_install) unattended-upgrades
+sudo apt install -y unattended-upgrades
 
 # firefox (remove snap version, install apt version)
 sudo snap remove firefox
-sudo add-apt-repository ppa:mozillateam/ppa
-$(apt_install) firefox
+sudo add-apt-repository -y ppa:mozillateam/ppa
+sudo apt install -y firefox
 echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
@@ -23,43 +25,43 @@ echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codenam
   sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 # various
-$(apt_install) aptitude blender curl dict dosfstools emacs exuberant-ctags git \
+sudo apt install -y aptitude blender curl dict dosfstools emacs exuberant-ctags git \
   htop imagemagick inkscape irssi manpages-dev ruby samba unrar unzip vim-gtk vlc \
   wbritish wine zip
 
 # asdf elixir
-$(apt_install) autoconf fop libgl1-mesa-dev libglu1-mesa-dev libmysqlclient-dev libncurses5-dev libpng-dev libpq-dev libreadline-dev libssh-dev libwxgtk3.0-dev m4 unixodbc-dev xsltproc
+sudo apt install -y autoconf fop libgl1-mesa-dev libglu1-mesa-dev libmysqlclient-dev libncurses5-dev libpng-dev libpq-dev libreadline-dev libssh-dev m4 unixodbc-dev xsltproc
 
 # asdf erlang
-$(apt_install) autoconf build-essential fop libgl1-mesa-dev libglu1-mesa-dev libncurses-dev libncurses5-dev libpng-dev libssh-dev libwxgtk-webview3.0-gtk3-dev libwxgtk3.0-gtk3-dev libxml2-utils m4 unixodbc-dev xsltproc
+sudo apt install -y autoconf build-essential fop libgl1-mesa-dev libglu1-mesa-dev libncurses-dev libncurses5-dev libpng-dev libssh-dev libwxgtk-webview3.0-gtk3-dev libwxgtk3.0-gtk3-dev libxml2-utils m4 unixodbc-dev xsltproc
 
 # asdf ruby
-$(apt_install) autoconf bison build-essential libdb-dev libffi-dev libgdbm-dev libgdbm6 libncurses5-dev libreadline6-dev libssl-dev libyaml-dev uuid-dev zlib1g-dev
+sudo apt install -y autoconf bison build-essential libdb-dev libffi-dev libgdbm-dev libgdbm6 libncurses5-dev libreadline6-dev libssl-dev libyaml-dev uuid-dev zlib1g-dev
 
 # bevy
-$(apt_install) g++ pkg-config libx11-dev libasound2-dev libudev-dev
+sudo apt install -y g++ pkg-config libx11-dev libasound2-dev libudev-dev
 
 # postgresql server
-$(apt_install) postgresql libpq-dev
+sudo apt install -y postgresql libpq-dev
 
 # ssh server
-$(apt_install) openssh-server
+sudo apt install -y openssh-server
 
 # htop
-$(apt_install) htop
+sudo apt install -y htop
 
 # diff-so-fancy
-sudo add-apt-repository ppa:aos1/diff-so-fancy
-$(apt_install) diff-so-fancy
+sudo add-apt-repository -y ppa:aos1/diff-so-fancy
+sudo apt install -y diff-so-fancy
 
 # github cli
-$(apt_install) gh
+sudo apt install -y gh
 
 # wmctrl (gvim wrapper)
-$(apt_install) wmctrl
+sudo apt install -y wmctrl
 
 # jq (json pretty printer / query)
-$(apt_install) jq
+sudo apt install -y jq
 
 # aws cli
-$(apt_install) awscli
+sudo apt install -y awscli
--- apt-work-ubuntu-22.sh
+++ apt-work-ubuntu-22.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-apt_install=sudo apt install -y --without-recommends
-
 # docker server
-$(apt_install) docker ca-certificates curl gnupg lsb-release
+sudo apt install -y docker ca-certificates curl gnupg lsb-release
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo usermod -aG docker greg
 
 # mysql server
-$(apt_install) mysql-server mysql-client-8.0 libmysqlclient-dev
+sudo apt install -y mysql-server mysql-client-8.0 libmysqlclient-dev
 
 # redis server
-$(apt_install) redis-server redis-tools
+sudo apt install -y redis-server redis-tools
