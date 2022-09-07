ec8eb22bd6125903a1ebbef08eeb0eed8025c868
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-07 11:52:59 +1000

    Apt packages

--- apt-ubuntu-22.sh
+++ apt-ubuntu-22.sh
@@ -65,3 +65,9 @@ sudo apt install -y jq
 
 # aws cli
 sudo apt install -y awscli
+
+# spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client
