10c05939252a808a0d874db36f5d0c765189ebd8
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-12 14:29:49 +1000

    Fix photo sync up script

--- backup/photo-sync-up.rb
+++ backup/photo-sync-up.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-system("aws s3 sync ./photos/ s3://puyofiles/photos/ --profile puyo --exclude '*' --include 'photos/*'")
+system("aws s3 sync ./photos/ s3://puyofiles/photos/ --profile puyo")
