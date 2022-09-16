120519d7d8293390712bb57346b0a63aee5188c8
Gregory McIntyre <greg@gregorymcintyre.com>
2022-09-16 11:06:41 +1000

    Add shellcheck to standard install process

--- apt-ubuntu-22.sh
+++ apt-ubuntu-22.sh
@@ -66,6 +66,9 @@ sudo apt install -y jq
 # aws cli
 sudo apt install -y awscli
 
+# shellcheck
+sudo apt install -y shellcheck
+
 # spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
