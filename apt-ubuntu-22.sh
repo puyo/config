f24a635cda4bf163e34348dd5e59446cfa008eed
Gregory McIntyre <greg@gregorymcintyre.com>
2022-10-13 07:40:37 +1100

    Tidy up list of apt packages

--- apt-ubuntu-22.sh
+++ apt-ubuntu-22.sh
@@ -14,8 +14,8 @@ sudo apt install -y unattended-upgrades
 
 # firefox (remove snap version, install apt version)
 sudo snap remove firefox
+sudo apt remove -y firefox
 sudo add-apt-repository -y ppa:mozillateam/ppa
-sudo apt install -y firefox
 echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
@@ -24,31 +24,92 @@ Pin-Priority: 1001
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | \
   sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
-# various
-sudo apt install -y \
-  aptitude \
-  blender \
-  curl \
-  dict \
-  dosfstools \
-  emacs \
-  exuberant-ctags \
-  git \
-  htop \
-  imagemagick \
-  inkscape \
-  irssi \
-  manpages-dev \
-  net-tools \
-  ruby \
-  samba \
-  unrar \
-  unzip \
-  vim-gtk \
-  vlc \
-  wbritish \
-  wine \
-  zip
+packages=(
+  aptitude                     # apt UI
+  avahi-daemon                 # dns discovery, mDNS
+  bash-completion              # shell tab completion
+  blender                      # 3D graphics
+  command-not-found            # suggest uninstalled programs
+  cups                         # printer support
+  curl                         # download over HTTP
+  dict                         # dictionary
+  dolphin                      # file manager
+  dosfstools                   # fix line endings
+  emacs                        # editor
+  exuberant-ctags              # code indexer
+  firefox                      # browser
+  fonts-hack                   # font
+  friendly-recovery            # recovery boot mode menu
+  geoip-database               # IP geo lookup
+  gh                           # github cli
+  git                          # version control
+  gwenview                     # image viewer
+  htop                         # task monitor
+  imagemagick                  # image conversion on command line
+  inkscape                     # vector graphics
+  iptables                     # firewall
+  iputils-tracepath            # souped up traceroute
+  irssi                        # irc client
+  jq                           # jq (json pretty printer / query)
+  kamera                       # digital camera support
+  kate                         # text editor
+  kcalc                        # calculator
+  kde-config-gtk-style         # gtk style config
+  kde-config-gtk-style-preview # gtk style config
+  kde-config-screenlocker      # screen lock config
+  kde-config-tablet            # wacom tablet support
+  kde-config-whoopsie          # configure error reporting to canonical
+  kde-spectacle                # screen capture app
+  kdeconnect                   # smartphone integration
+  kerneloops                   # report kernal crashes
+  kwalletmanager               # manage passwords
+  manpages-dev                 # manual pages for devs
+  mesa-utils                   # check 3D acceleration is working
+  mesa-vulkan-drivers          # faster 3D drivers
+  net-tools                    # ifconfig and similar command line tools
+  network-manager              # wifi connection manager
+  okular                       # pdf and document viewer
+  okular-extra-backends        # okular epub support
+  openssh-server               # ssh into this machine
+  p7zip-full                   # decommpress files
+  partitionmanager             # manage partitions
+  pavucontrol-qt               # pulseaudio volume control
+  pipewire                     # multimedia server
+  plasma-browser-integration   # media control, download notifications
+  plasma-discover              # software library
+  plasma-disks                 # alert about failing S.M.A.R.T. hard disks
+  plasma-firewall              # firewall graphical config
+  plasma-systemmonitor         # show sensor info
+  plasma-thunderbolt           # manage thunderbolt devices
+  plasma-vault                 # manage encrypted folders
+  plasma-widgets-addons        # various widgets including nightcolor control
+  plymouth                     # boot logo
+  plymouth-theme-spinner       # basic boot logo theme
+  pm-utils                     # suspend/hibernate from command line
+  print-manager                # KDE printer manager
+  pulseaudio-module-bluetooth  # play audio on bluetooth
+  ruby                         # interpreter for many of my scripts
+  samba                        # share files with windows machines on the network
+  secureboot-db                # manage secureboot database
+  shellcheck                   # shell check
+  smbclient                    # windows file sharing
+  telnet                       # test networks
+  thermald                     # monitor computer temperature
+  unrar                        # decompress files
+  unzip                        # decompress files
+  vim-gtk                      # editor
+  vlc                          # media player
+  vulkan-tools                 # vulkaninfo
+  wbritish                     # british dictionary
+  wine                         # windows emulator
+  wmctrl                       # wmctrl (gvim wrapper)
+  xserver-xorg-video-all       # video card drivers (does not include intel)
+  xserver-xorg-video-intel     # video card drivers for intel
+  xsettingsd                   # gnome app support in non-gnome environment
+  zip                          # decompress files
+)
+
+sudo apt install -y "${packages[@]}"
 
 # asdf elixir
 sudo apt install -y autoconf fop libgl1-mesa-dev libglu1-mesa-dev libmysqlclient-dev libncurses5-dev libpng-dev libpq-dev libreadline-dev libssh-dev m4 unixodbc-dev xsltproc
@@ -65,27 +126,9 @@ sudo apt install -y g++ pkg-config libx11-dev libasound2-dev libudev-dev
 # postgresql server
 sudo apt install -y postgresql libpq-dev
 
-# ssh server
-sudo apt install -y openssh-server
-
-# htop
-sudo apt install -y htop
-
 # diff-so-fancy
 sudo add-apt-repository -y ppa:aos1/diff-so-fancy
 sudo apt install -y diff-so-fancy
-
-# github cli
-sudo apt install -y gh
-
-# wmctrl (gvim wrapper)
-sudo apt install -y wmctrl
-
-# jq (json pretty printer / query)
-sudo apt install -y jq
-
-# shell check
-sudo apt install -y shellcheck
 
 # spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
@@ -112,9 +155,3 @@ output-mappings = hdmi-stereo analog-stereo
 input-mappings = analog-stereo
 ' >> /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf"
 fi
-
-sudo apt install -y \
-  okular \
-  command-not-found \
-  avahi-daemon \
-  bash-completion
