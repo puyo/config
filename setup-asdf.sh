aae30866ca36ed6d381a66549e73b9fcc2590ed3
Gregory McIntyre <greg@gregorymcintyre.com>
2022-11-03 15:34:55 +1100

    Python 3.10.0 by default, update asdf setup scripts

--- setup-asdf.sh
+++ setup-asdf.sh
@@ -2,6 +2,20 @@
 
 set -xeuo pipefail
 
+# asdf elixir
+sudo apt install -y autoconf fop libgl1-mesa-dev libglu1-mesa-dev libmysqlclient-dev libncurses5-dev libpng-dev libpq-dev libreadline-dev libssh-dev m4 unixodbc-dev xsltproc
+
+# asdf erlang
+sudo apt install -y autoconf build-essential fop libgl1-mesa-dev libglu1-mesa-dev libncurses-dev libncurses5-dev libpng-dev libssh-dev libwxgtk-webview3.0-gtk3-dev libwxgtk3.0-gtk3-dev libxml2-utils m4 unixodbc-dev xsltproc
+
+# asdf ruby
+sudo apt install -y autoconf bison build-essential libdb-dev libffi-dev libgdbm-dev libgdbm6 libncurses5-dev libreadline6-dev libssl-dev libyaml-dev uuid-dev zlib1g-dev
+
+# asdf python
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2 || true
 
 source ~/.asdf/asdf.sh
