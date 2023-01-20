e64598f14c19c904aad16c440a3f3b0be6120f61
Gregory McIntyre <greg@gregorymcintyre.com>
2023-01-20 11:28:56 +1100

    Accept insecure TLS handshakes for sad old websites like the VPN

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,25 +1,19 @@
 /*** MY OVERRIDES ***/
 user_pref("_user.js.parrot", "overrides section syntax error");
 
-// user_pref("privacy.cpd.cache", true);    // [DEFAULT: true]
-// user_pref("privacy.cpd.cookies", false); // 2812 Ctrl-Shift-Del
-// user_pref("privacy.cpd.formdata", true); // [DEFAULT: true]
-// user_pref("privacy.cpd.history", false); // 2812 to match when you use Ctrl-Shift-Del
-// user_pref("privacy.cpd.offlineApps", false);
-// user_pref("privacy.cpd.offlineApps", false); // [DEFAULT: false]
-// user_pref("privacy.cpd.sessions", true); // [DEFAULT: true]
-user_pref("browser.display.use_system_colors", true); // [DEFAULT: false NON-WINDOWS]
+user_pref("browser.display.use_system_colors", true);
 user_pref("browser.formfill.enable", true);
-user_pref("browser.startup.page", 3); // 0102
+user_pref("browser.startup.page", 3);
 user_pref("keyword.enabled", true);
-user_pref("network.cookie.lifetimePolicy", 0); // 2801
-user_pref("privacy.clearOnShutdown.cache", false);     // 2811
-user_pref("privacy.clearOnShutdown.cookies", false); // 2811
-user_pref("privacy.clearOnShutdown.downloads", false); // 2811
-user_pref("privacy.clearOnShutdown.formdata", false);  // 2811
-user_pref("privacy.clearOnShutdown.history", false); // 2811
-user_pref("privacy.clearOnShutdown.offlineApps", false); // 2811
-user_pref("privacy.clearOnShutdown.sessions", false);  // 2811
+user_pref("network.cookie.lifetimePolicy", 0);
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false);
+user_pref("privacy.clearOnShutdown.formdata", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("security.ssl.require_safe_negotiation", false);
 user_pref("signon.autofillForms", true);
-user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
