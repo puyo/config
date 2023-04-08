25f2b8d6945e4008df497abcf52641ac4193dd9d
Gregory McIntyre <greg@gregorymcintyre.com>
2023-04-08 17:05:07 +1000

    Customise doom

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(centaur-tabs-set-bar 'over))
+ '(centaur-tabs-set-bar 'over)
+ '(package-selected-packages '(counsel cmake-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-code-face ((t (:extend t :family "Source Code Pro")))))
+ '(font-lock-comment-face ((t (:foreground "LightSteelBlue3" :slant italic))))
+ '(markdown-code-face ((t (:extend t :family "Source Code Pro"))))
+ '(region ((t (:extend t :background "#4e4f5a")))))
