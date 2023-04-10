d45c911481dc7c95b8900a273849c918155a086c
Gregory McIntyre <greg@gregorymcintyre.com>
2023-04-10 21:30:28 +1000

    Improve doom markdown keybinds

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(centaur-tabs-set-bar 'over)
- '(package-selected-packages '(counsel cmake-mode)))
+ '(package-selected-packages '(counsel cmake-mode))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -12,6 +12,6 @@
  ;; If there is more than one, they won't work right.
  '(font-lock-comment-face ((t (:foreground "LightSteelBlue3" :slant italic))))
  '(line-number ((t (:inherit default :foreground "#585a64" :slant italic :weight normal))))
- '(markdown-code-face ((t (:extend t :family "Source Code Pro"))))
  '(mode-line-inactive ((t (:background "#1a1c23" :foreground "#6e7079" :box nil))))
- '(region ((t (:extend t :background "#4e4f5a")))))
+ '(region ((t (:extend t :background "#4e4f5a"))))
+ )
