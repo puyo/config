aafa69e7c2af3e0fdd303facf21c725cd52b18c9
Gregory McIntyre <greg@gregorymcintyre.com>
2023-04-11 11:14:18 +1000

    More packages for doom emacs

--- setup-kubuntu-22.sh
+++ setup-kubuntu-22.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 # no recommends kthx
-sudo tee /etc/apt/apt.conf.d/99_norecommends << 'EOF'
+sudo tee /etc/apt/apt.conf.d/99_norecommends <<'EOF'
 APT::Install-Recommends "false";
 APT::AutoRemove::RecommendsImportant "false";
 APT::AutoRemove::SuggestsImportant "false";
@@ -18,55 +18,59 @@ Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | \
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' |
   sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 packages=(
-  alsa-ucm-conf                 # needed for output to audio device plugged into screen
-  apparmor                      # security policies
-  aptitude                      # apt UI
-  at-spi2-core                  # prevents a warning in emacs start up
-  bash-completion               # shell tab completion
-  blender                       # 3D graphics
-  cmake                         # needed to build some gems
-  curl                          # download over HTTP
-  dict                          # dictionary
-  dosfstools                    # fix line endings
-  emacs                         # editor
-  exuberant-ctags               # code indexer
-  ffmpeg                        # work with videos
-  firefox                       # browser
-  firmware-sof-signed           # intel audio drivers
-  fonts-noto                    # unicode characters
-  gh                            # github cli
-  git                           # version control
-  htop                          # task monitor
-  imagemagick                   # image conversion on command line
-  inkscape                      # vector graphics
-  inotify-tools                 # Elixir Phoenix uses this
-  jq                            # jq (json pretty printer / query)
-  less                          # terminal pager
-  manpages-dev                  # manual pages for devs
-  mesa-utils                    # check 3D acceleration is working
-  mesa-vulkan-drivers           # faster 3D drivers
-  mtr-tiny                      # ping/traceroute
-  net-tools                     # ifconfig and similar command line tools
-  openssh-server                # ssh into this machine
-  partitionmanager              # manage partitions
-  plymouth-theme-spinner        # basic boot logo theme
-  pm-utils                      # suspend/hibernate from command line
-  ruby                          # interpreter for many of my scripts
-  shellcheck                    # shell check
-  strace                        # debug programs
-  telnet                        # test networks
-  unattended-upgrades           # unattended security upgrades
-  vim-gtk                       # editor
-  vlc                           # media player
-  vulkan-tools                  # vulkaninfo
-  wbritish                      # british dictionary
-  whois                         # domain name info
-  wine                          # windows emulator
-  wmctrl                        # wmctrl (gvim wrapper)
+  alsa-ucm-conf          # needed for output to audio device plugged into screen
+  apparmor               # security policies
+  aptitude               # apt UI
+  at-spi2-core           # prevents a warning in emacs start up
+  bash-completion        # shell tab completion
+  blender                # 3D graphics
+  cmake                  # needed to build some gems
+  curl                   # download over HTTP
+  dict                   # dictionary
+  dosfstools             # fix line endings
+  editorconfig           # used by Doom Emacs
+  emacs                  # editor
+  exuberant-ctags        # code indexer
+  fd-find                # used by Doom Emacs
+  ffmpeg                 # work with videos
+  firefox                # browser
+  firmware-sof-signed    # intel audio drivers
+  fonts-noto             # unicode characters
+  gh                     # github cli
+  git                    # version control
+  htop                   # task monitor
+  imagemagick            # image conversion on command line
+  inkscape               # vector graphics
+  inotify-tools          # Elixir Phoenix uses this
+  jq                     # jq (json pretty printer / query)
+  less                   # terminal pager
+  manpages-dev           # manual pages for devs
+  mesa-utils             # check 3D acceleration is working
+  mesa-vulkan-drivers    # faster 3D drivers
+  mtr-tiny               # ping/traceroute
+  net-tools              # ifconfig and similar command line tools
+  openssh-server         # ssh into this machine
+  partitionmanager       # manage partitions
+  plymouth-theme-spinner # basic boot logo theme
+  pm-utils               # suspend/hibernate from command line
+  ripgrep                # used by Doom Emacs
+  ruby                   # interpreter for many of my scripts
+  shellcheck             # shell check
+  shfmt                  # used by Doom Emacs
+  strace                 # debug programs
+  telnet                 # test networks
+  unattended-upgrades    # unattended security upgrades
+  vim-gtk                # editor
+  vlc                    # media player
+  vulkan-tools           # vulkaninfo
+  wbritish               # british dictionary
+  whois                  # domain name info
+  wine                   # windows emulator
+  wmctrl                 # wmctrl (gvim wrapper)
 )
 
 sudo apt install -y "${packages[@]}"
@@ -91,7 +95,7 @@ sudo apt install spotify-client
 
 # albert
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg >/dev/null
 sudo apt update
 sudo apt install albert
 
