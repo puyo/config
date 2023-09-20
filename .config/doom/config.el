b25b51e2f32f379aa929ff096bfaeb385339bda1
Gregory McIntyre <greg@gregorymcintyre.com>
2023-09-20 14:25:55 +1000

    Copilot

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -346,3 +346,15 @@
   (add-to-list 'exec-path "~/.elixir-ls"))
 
 (setq flycheck-elixir-credo-strict t)
+
+;; ----------------------------------------------------------------------
+;; Copilot
+
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
