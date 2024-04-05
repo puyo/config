573940047fb4330c6410a0eaf9cf49fedacd4071
Gregory McIntyre <greg@gregorymcintyre.com>
2024-04-05 11:16:41 +1100

    Ditch copilot, feels bad using so much water

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -368,13 +368,13 @@
 ;; Copilot
 
 ;; accept completion from copilot and fallback to company
-(use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+;; (use-package! copilot
+;;   :hook (prog-mode . copilot-mode)
+;;   :bind (:map copilot-completion-map
+;;               ("<tab>" . 'copilot-accept-completion)
+;;               ("TAB" . 'copilot-accept-completion)
+;;               ("C-TAB" . 'copilot-accept-completion-by-word)
+;;               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
 ;; ----------------------------------------------------------------------
 ;; JavaScript
