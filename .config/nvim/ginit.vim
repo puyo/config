265c54ccbb495f7b4b3556fdf6d7c49876a552d5
Gregory McIntyre <greg@gregorymcintyre.com>
2024-04-12 14:40:55 +1000

    Neovim config update

--- .config/nvim/ginit.vim
+++ .config/nvim/ginit.vim
@@ -3,3 +3,5 @@ set clipboard+=unnamedplus
 set guifont=ComicMono\ Nerd\ Font:h13
 set wildmenu
 set wildmode=list,longest,full
+
+call rpcnotify(0, 'Gui', 'Option', 'Tabline', 0)
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -11,20 +11,56 @@ local function edit_current_buffer_dir_expr()
   return ":e " .. current_buffer_dir() .. "/"
 end
 
+local function edit_last_file_expr()
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local current_file = vim.api.nvim_buf_get_name(current_buffer)
+
+  for _, file in ipairs(vim.v.oldfiles) do
+    local file_stat = vim.loop.fs_stat(file)
+    if file_stat and file_stat.type == "file" and file ~= current_file then
+      return ":e " .. file .. "<cr>"
+    end
+  end
+
+  return ""
+end
+
+-- Keep oldfiles up to date so we can switch between them
+vim.cmd([[
+function! UpdateOldFiles()
+  let file = expand('%:p')
+  let idx = index(v:oldfiles, file)
+  if idx != -1
+    call remove(v:oldfiles, idx)
+  endif
+  call insert(v:oldfiles, file, 0)
+endfunction
+autocmd! BufEnter * call UpdateOldFiles()
+]])
+
 local map = vim.keymap.set
 local telescope_builtin = require("telescope.builtin")
+local tabufline = require("nvchad.tabufline")
+local comment = require("Comment.api")
 
 map("c", "<M-BS>", "<C-W>", { desc = "Back word" })
 map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map({ "n", "i", "v" }, "<D-b>", telescope_builtin.buffers, { desc = "Buffers" })
+map({ "n", "i" }, "<D-/>", comment.toggle.linewise.current, { desc = "Toggle comment" })
+map({ "n", "i", "v" }, "<D-b>", telescope_builtin.buffers, { desc = "Browse buffers" })
 map({ "n", "i", "v" }, "<D-o>", edit_current_buffer_dir_expr, { desc = "Open buffer", expr = true })
-map({ "n", "i", "v" }, "<D-p>", telescope_builtin.git_files, { desc = "Project files" })
-map({ "n", "i", "v" }, "<D-r>", telescope_builtin.oldfiles, { desc = "Recent files" })
+map({ "n", "i", "v" }, "<D-p>", telescope_builtin.git_files, { desc = "Browse project files" })
+map({ "n", "i", "v" }, "<D-r>", telescope_builtin.oldfiles, { desc = "Browse recent files" })
 map({ "n", "i", "v" }, "<D-s>", "<cmd>w<cr>", { desc = "Save buffer" })
-map({ "n", "i", "v" }, "<D-w>", "<cmd>silent confirm bd<cr>", { desc = "Close buffer" })
-map({ "n", "i", "v" }, "<D-z>", "<cmd>silent undo<cr>", { desc = "Undo" })
-map({ "n", "i" }, "<D-/>", require("Comment.api").toggle.linewise.current, { desc = "Comment toggle" })
+map({ "n", "i", "v" }, "<D-w>", tabufline.close_buffer, { desc = "Close buffer" })
+map({ "n", "i", "v" }, "<D-z>", "<cmd>undo<cr>", { desc = "Undo", silent = true })
+map({ "n", "i", "v" }, "<D-{>", tabufline.prev, { desc = "Go to prev buffer" })
+map({ "n", "i", "v" }, "<D-}>", tabufline.next, { desc = "Go to next buffer" })
+map({ "n", "i", "v" }, "<leader><tab>", edit_last_file_expr, { desc = "Go to last buffer", expr = true, silent = true })
+
+-- stop nvchad messing with C-i
+vim.keymap.set("n", "<tab>", "")
+vim.keymap.del("n", "<tab>")
 
 -- open current code in github, gitlab, etc., browser
 
