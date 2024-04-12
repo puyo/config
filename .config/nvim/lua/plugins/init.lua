18938692e1b6c24cf82c643328a5302738f756b5
Gregory McIntyre <greg@gregorymcintyre.com>
2024-04-12 14:40:55 +1000

    Neovim config update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -50,11 +50,22 @@ return {
 
   {
     "ahmedkhalf/project.nvim",
+    lazy = false,
     config = function()
+      require("nvim-tree").setup({
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
+      })
       require("project_nvim").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        manual = false,
+        detection_methods = { "pattern" },
+        patterns = { ".git", "Gemfile", "package.json", "cargo.toml" },
+        silent_chdir = true,
+        show_hidden = true,
       })
     end,
   },
