6775d94ecdf3686b94a73e1ebf3bf67110ab9bfb
Gregory McIntyre <greg@gregorymcintyre.com>
2024-04-15 13:11:29 +1000

    nvim config update

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,8 +6,41 @@ o.showtabline = 0
 o.spelllang = "en_au"
 o.expandtab = true
 o.linespace = 2
+o.shiftwidth = 2
 
--- no really
 vim.cmd([[
-autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=0
+" no really, no tabline please
+" autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=0
+
+augroup filetypedetect
+au BufNewFile,BufRead *.{rjs,rbw,gem,gemspec,ru} setlocal filetype=ruby
+au BufNewFile,BufRead {Gemfile,Guardfile} setlocal filetype=ruby
+au BufNewFile,BufRead *.txt setlocal filetype=text
+au BufNewFile,BufRead *.ejs setlocal filetype=html
+au BufNewFile,BufRead *.hamlc setlocal filetype=haml
+au BufNewFile,BufRead *.{md,md.erb,markdown.liquid} setlocal filetype=markdown conceallevel=2 | hi mkdCode guibg=NONE
+au BufNewFile,BufRead *.as setlocal filetype=javascript
+au BufNewFile,BufRead Dockerfile.* setlocal filetype=dockerfile
+au BufRead,BufNewFile *.ex,*.exs set filetype=elixir
+au BufRead,BufNewFile *.eex,*.heex,*.leex,*.sface,*.lexs set filetype=eelixir
+au BufRead,BufNewFile mix.lock set filetype=elixir
+augroup END
+
+augroup filetypes
+au FileType c setlocal sw=4 sts=4 makeprg=make
+au FileType ruby setlocal makeprg=rake path+=lib tw=78 ts=2 et sw=2 sts=2
+au FileType ruby let b:surround_114 = "do\n\r\nend"
+au FileType eruby setlocal makeprg=rake
+au FileType css setlocal makeprg=rake
+au FileType text setlocal wrap linebreak nolist tw=0 wm=0 spell
+au FileType python setlocal et ts=4 sw=4 sts=4
+au FileType plaintex setlocal spell
+au FileType markdown setlocal iskeyword-=/ wrap linebreak nolist sw=2 tw=0 wm=0 spell
+au FileType slim setlocal comments+=b:'
+au FileType coffee setlocal ts=2 sw=2 sts=2
+au FileType javascript setlocal ts=2 sw=2 sts=2
+au FileType typescript setlocal ts=2 sw=2 sts=2
+au FileType typescriptreact setlocal ts=2 sw=2 sts=2
+au FileType json setlocal nowrap smartindent
+augroup END
 ]])
