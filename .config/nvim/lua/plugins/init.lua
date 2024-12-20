060644a4b663ea1f7447f09da9e1e6308c1dbd76
Gregory McIntyre <greg@gregorymcintyre.com>
2024-12-20 21:10:58 +1100

    Outline package in nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -436,4 +436,16 @@ return {
       disable_frontmatter = true,
     },
   },
+
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
+  },
 }
