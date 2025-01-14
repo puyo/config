70001c6fbeb027782c61ee35d7a848250a247566
Gregory McIntyre <greg@gregorymcintyre.com>
2025-01-15 10:14:24 +1100

    Autoload buffer if file changes on disk, nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,8 +32,8 @@ dofile(vim.g.base46_cache .. "statusline")
 require("options")
 require("nvchad.autocmds")
 require("configs.auto_make_dir")
+require("configs.autoread")
 
 vim.schedule(function()
   require("mappings")
 end)
-
