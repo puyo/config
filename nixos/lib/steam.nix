07a24a3ba08043333de8efc2071102875d9a53a1
Gregory McIntyre <greg@gregorymcintyre.com>
2025-05-28 17:46:09 +1000

    nixos: steam-run and other steam stuff

--- nixos/lib/steam.nix
+++ nixos/lib/steam.nix
@@ -1,4 +1,23 @@
 {
-  nixpkgs.config.allowUnfree = true;
-  programs.steam.enable = true;
+  config,
+  pkgs,
+  ...
+}: {
+  config = {
+    nixpkgs.config.allowUnfree = true;
+
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+
+    environment.systemPackages = with pkgs; [
+      steam-run
+    ];
+
+    environment.sessionVariables = {
+      WLR_NO_HARDWARE_CURSORS = "1";
+    };
+  };
 }
