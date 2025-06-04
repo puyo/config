32f1233657fd339f3583d4e5a625d9d725534ab1
Gregory McIntyre <greg@gregorymcintyre.com>
2025-06-05 09:47:13 +1000

    Hopefully fix NixOS networkmanager limited connectivity warning

--- nixos/lib/common.nix
+++ nixos/lib/common.nix
@@ -1,5 +1,6 @@
 {
   networking.networkmanager.enable = true;
+  networking.networkmanager.settings.connectivity.uri = "http://nmcheck.gnome.org/check_network_status.txt";
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
