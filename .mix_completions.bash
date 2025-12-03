12a66ab1588d24728fb286e66913694cf1f6ee03
Gregory McIntyre <greg@gregorymcintyre.com>
2025-12-04 07:22:25 +1100

    Update mix completions script

--- .mix_completions.bash
+++ .mix_completions.bash
@@ -1,117 +1,40 @@
-__mix_completion_debug() {
-  if [[ -n ${BASH_COMP_DEBUG_FILE-} ]]; then
-    echo "$*" >>"${BASH_COMP_DEBUG_FILE}"
+_mix() {
+  if ! type mix &> /dev/null || ! [ -e mix.exs ]; then
+    return
   fi
-}
 
-__mix_completion_handle_standard_completion_case() {
-  local tab=$'	' comp
+  local current_mix_md5
 
-  local longest=0
-  local compline
-  # Look for the longest completion so that we can format things nicely
-  while IFS='' read -r compline; do
-    [[ -z $compline ]] && continue
-    # Strip any description before checking the length
-    comp=${compline%%$tab*}
-    # Only consider the completions that match
-    [[ $comp == "$cur"* ]] || continue
-    COMPREPLY+=("$compline")
-    if ((${#comp} > longest)); then
-      longest=${#comp}
-    fi
-  done <<<"$completions"
-
-  # If there is a single completion left, remove the description text
-  if ((${#COMPREPLY[*]} == 1)); then
-    comp="${COMPREPLY[0]%%$tab*}"
-    COMPREPLY[0]=$comp
-  else # Format the descriptions
-    __mix_completion_format_comp_descriptions $longest
-  fi
-}
-
-__mix_completion_format_comp_descriptions() {
-  local tab=$'	'
-  local comp desc maxdesclength
-  local longest=$1
-
-  local i ci
-  for ci in ${!COMPREPLY[*]}; do
-    comp=${COMPREPLY[ci]}
-    # Properly format the description string which follows a tab character if there is one
-    if [[ "$comp" == *$tab* ]]; then
-      desc=${comp#*$tab}
-      comp=${comp%%$tab*}
-
-      # $COLUMNS stores the current shell width.
-      # Remove an extra 4 because we add 2 spaces and 2 parentheses.
-      maxdesclength=$((COLUMNS - longest - 4))
-
-      # Make sure we can fit a description of at least 8 characters
-      # if we are to align the descriptions.
-      if ((maxdesclength > 8)); then
-        # Add the proper number of spaces to align the descriptions
-        for ((i = ${#comp}; i < longest; i++)); do
-          comp+=" "
-        done
-      else
-        # Don't pad the descriptions so we can fit more text after the completion
-        maxdesclength=$((COLUMNS - ${#comp} - 4))
-      fi
-
-      # If there is enough space for any description text,
-      # truncate the descriptions that are too long for the shell width
-      if ((maxdesclength > 0)); then
-        if ((${#desc} > maxdesclength)); then
-          desc=${desc:0:$((maxdesclength - 1))}
-          desc+="…"
-        fi
-        comp+="  ($desc)"
-      fi
-      COMPREPLY[ci]=$comp
-    fi
-  done
-}
-
-_mix_complete_complete() {
-  local options
-  local IFS=$' 	
-'
-
-  local words=("${COMP_WORDS[@]}")
-  local cword=$COMP_CWORD
-  local prev=$3
-  local cur="${words[$cword]}"
-
-  COMPREPLY=()
-
-  if [[ -f 'mix.exs' && -f '.mix_complete.cache' ]]; then
-    options=$(cat .mix_complete.cache)
-  elif [[ ! -z "$XDG_CACHE_HOME" && -f "$XDG_CACHE_HOME/.mix_complete.cache" ]]; then
-    options=$(cat "$XDG_CACHE_HOME/.mix_complete.cache")
-  elif [[ -f "$HOME/.cache/.mix_complete.cache" ]]; then
-    options=$(cat "$HOME/.cache/.mix_complete.cache")
+  if type "md5" &>/dev/null; then
+    current_mix_md5=`md5 -q mix.exs`
+  elif type "md5sum" &>/dev/null; then
+    current_mix_md5=`md5sum mix.exs | awk '{ print $1 }'`
   else
-    options=""
+    # no either md5 or md5sum
+    return 1
   fi
 
-  completions="$options"
-
-  __mix_completion_debug "==$(date)=="
-  __mix_completion_debug "cur $cur"
-  __mix_completion_debug "words $words"
-  if [[ $prev == 'mix' ]]; then
-    __mix_completion_handle_standard_completion_case
-  else
-    COMPREPLY=()
-    compopt -o default
+  if [[ $current_mix_md5 != $_last_mix_md5 ]]; then
+    mix compile >/dev/null 2>&1
+    if [[ $? -eq 1 ]]; then
+      # Project compilation failed --
+      # show a message explaining why we can't show completions?
+      return 1
+    fi
+    _last_mix_md5=$current_mix_md5
+    _mix_task_list="$(
+      mix run -e '
+        Mix.Task.load_all
+          |> Enum.map(&(Mix.Task.task_name &1))
+          |> Enum.sort
+          |> Enum.join(" ")
+          |> IO.puts
+      '
+    )"
   fi
+
+  cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$_mix_task_list" $cur) )
+  return 0
 }
-
-_mix_complete_setup() {
-  complete -F _mix_complete_complete mix
-}
-
-_mix_complete_setup
-
+complete -o bashdefault -o default -o nospace -F _mix mix
